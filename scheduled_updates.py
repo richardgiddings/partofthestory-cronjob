6a896143ea9b687bea4d456fb8db281d4cbb2da8
Richard Giddings <rmpgiddings@gmail.com>
2025-11-17 14:58:49 +0000

    Various improvements to the script

--- scheduled_updates.py
+++ scheduled_updates.py
@@ -9,35 +9,25 @@ DB_PORT     = config('DB_PORT')
 
 DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
+"""
+BRIEF SUMMARY
+
+We want to delete story parts that were started
+more than 3 days ago but have not been completed and make the story 
+available again.
+
+To do this we:
+- Identify the stories involved
+- If these stories have completed parts we want to make the story available again
+- Delete the parts that have not been completed in 3 days
+- If the stories only had the one part just delete the story too
+"""
+
 with psycopg.connect(DATABASE_URL) as conn:
 
     with conn.cursor() as cur:
 
-        # if a story has parts that have not been completed in 3 days
-        # and there are parts that have been completed (last_user_id exists)
-        # then make the story available for editing
-        cur.execute(
-            """
-            UPDATE story 
-               SET locked = false 
-             WHERE 
-                id IN (
-                    SELECT story_id 
-                    FROM part 
-                    WHERE 
-                        date_complete IS NULL AND 
-                        user_id IS NOT NULL AND  
-                        date_started < NOW() - INTERVAL '3 days'
-                )
-                AND last_user_id IS NOT NULL;
-            """
-        )
-        print(f"{cur.rowcount} stories were updated")
-
-        # if a story has parts that have not been completed in 3 days
-        # but there are no parts that have been completed (last_user_id is null) 
-        # then just remove the story, for now just store the story_ids as we 
-        # can't delete until we have removed the part due to the foreign key
+        # The story_id of expired parts
         cur.execute(
             """
             SELECT 
@@ -50,33 +40,60 @@ with psycopg.connect(DATABASE_URL) as conn:
                 date_started < NOW() - INTERVAL '3 days'
             """
         )
-        delete_these_stories = [record[0] for record in cur]
+        story_ids_of_expired_parts = [record[0] for record in cur]
+        print(f"Stories ids with expired parts {story_ids_of_expired_parts}")
 
-        # now that we have updated the stories delete the latest part for the
-        # story that hasn't been finished in 3 days
-        cur.execute(
+
+        # If a story's latest part has not been completed in 3 days
+        # and there are parts that have been completed (last_user_id exists)
+        # then make the story available for editing
+        story_update_query = """
+            UPDATE 
+                story 
+            SET 
+                locked = false 
+            WHERE 
+                id = ANY(%s) AND
+                last_user_id IS NOT NULL;
             """
-            DELETE 
-              FROM part 
-             WHERE 
+        result = psycopg.ClientCursor(conn).mogrify(
+            story_update_query, (story_ids_of_expired_parts,)
+        )
+        cur.execute(story_update_query, (story_ids_of_expired_parts,))
+        print(f"{cur.rowcount} stories were updated using:")
+        print(result)
+
+
+        # Delete the latest part for the
+        # story that hasn't been finished in 3 days
+        part_delete_query = """
+            DELETE FROM 
+                part 
+            WHERE 
                 date_complete IS NULL AND 
                 user_id IS NOT NULL AND 
                 date_started < NOW() - INTERVAL '3 days';
             """
+        result = psycopg.ClientCursor(conn).mogrify(
+            part_delete_query
         )
-        print(f"{cur.rowcount} parts were deleted")
+        cur.execute(part_delete_query)
+        print(f"{cur.rowcount} parts were deleted using:")
+        print(result)
 
-        # now we have deleted the parts we can delete the stories
-        # note the addition of "last_user_id IS NULL" as we only want
-        # the stories that have no saved parts
-        cur.execute(
-            """
-            DELETE 
-              FROM story 
-             WHERE 
-                id IN (%s) AND
+
+        # Now we have deleted the parts we can delete the stories with
+        # only one part. Note the addition of "last_user_id IS NULL".
+        story_delete_query = """
+            DELETE FROM 
+                story 
+            WHERE 
+                id = ANY(%s) AND
                 last_user_id IS NULL;
-            """,
-            delete_these_stories
+            """
+        result = psycopg.ClientCursor(conn).mogrify(
+            story_delete_query, (story_ids_of_expired_parts,)
         )
-        print(f"{cur.rowcount} stories were deleted as there were no finished parts")
+        cur.execute(story_delete_query, (story_ids_of_expired_parts,))
+        print(f"{cur.rowcount} stories were deleted as there were no finished parts using:")
+        print(result)
